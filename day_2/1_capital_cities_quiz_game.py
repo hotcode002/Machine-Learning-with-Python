5d70f951b7b9440c8112d84e5b05b3f28ee50765
Ajay Tech <siva@ajaytech.co>
2018-11-06 01:05:17 +0530

    capital cities quiz game - version 1

---
+++ day_2/1_capital_cities_quiz_game.py
@@ -0,0 +1,27 @@
+import random
+
+q = {"The capital of India is New Delhi": True,
+     "The capital of China is Shanghai" : False,
+     "The capital of Australia is Canberra" : True,
+     "The capital of Brazil is Rio de Janiera": False,
+     "The capital of United States is New York" : False,
+     "The capital of Russia is St. Petersburg" : False,
+     "The capital of Singapore is Singapore" : True
+    }
+
+def str_to_bool (s ) :
+    if s == "true" or s == "True" :
+        return True
+    elif s == "false" or s == "False" :
+        return False
+
+
+while ( True ) : 
+    question = random.choice( list(q.keys()))
+    answer = input ( question + " - ")
+    answer.capitalize()
+    answer = str_to_bool ( answer)
+    if answer == q.get(question) :
+        print ( " correct ")
+    else : 
+        print ( " incorrect ")
