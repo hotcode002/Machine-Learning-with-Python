dd1bc14749e41bf1647ce57e9079c0b8a5d6be0d
Ajay Tech <siva@ajaytech.co>
2018-11-06 16:37:41 +0530

    asian countries > 100 M

--- day_2/3_asian_countries.py
+++ day_2/3_asian_countries.py
@@ -1,14 +1,40 @@
+# List all the Asian countries with population > 100 M 
 import csv
 
-file = open( "./data/asia_countries.csv" )
-asia_countries_f = csv.reader ( file )
+file_1 = open( "./data/asia_countries.csv" )
+asia_countries_f = csv.reader ( file_1 )
 asia_countries = []
 
 for row in asia_countries_f : 
     asia_countries.append ( row [0])
 
-print ( asia_countries )
-del asia_countries[0] 
-asia_countries.remove(0)
-print ( asia_countries )
+file_2 = open( "./data/countries_population.csv" )
+countries_population_f = csv.reader ( file_2 )
+countries_population = {}
+
+for row in countries_population_f : 
+    countries_population [ row[0] ] = row[1]
+
+print ( len (countries_population.keys()) )
+
+# Remove all countries in countries_population < 20 Million
+countries_population.pop('country')
+for key in list(countries_population.keys()) :
+    if int(countries_population[key]) < 100000000 : 
+        del countries_population[key]
+
+print ( len (countries_population.keys()) )
+
+print (countries_population)
+
+asia_countries_pop_100m = []
+
+for key in list(countries_population.keys())  :
+    if asia_countries.count(key) == 0:
+        del countries_population[key]
+
+print ( len (countries_population.keys()) )
+
+print (countries_population)
+
     
