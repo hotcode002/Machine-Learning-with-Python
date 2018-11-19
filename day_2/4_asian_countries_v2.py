4436063482a65e454a451da0ed20a2b3e325b4bf
Ajay Tech <siva@ajaytech.co>
2018-11-19 19:14:02 +0530

    us states

--- day_2/4_asian_countries_v2.py
+++ day_2/4_asian_countries_v2.py
@@ -15,28 +15,14 @@ countries_population = {}
 for row in countries_population_f : 
     countries_population [ row[0] ] = row[1]
 
-print ( len (countries_population.keys()) )
-
-print ( countries_population )
-print ( asia_countries)
 # Remove all countries in countries_population < 20 Million
 countries_population.pop('country')
 for key in list(countries_population.keys()) :
     if int(countries_population[key]) < 100000000 : 
         del countries_population[key]
 
-print ( len (countries_population.keys()) )
-
-print (countries_population)
-
 asia_countries_pop_100m = []
 
 for key in list(countries_population.keys())  :
     if asia_countries.count(key) == 0:
-        del countries_population[key]
-
-print ( len (countries_population.keys()) )
-
-print (countries_population)
-
-    
+        del countries_population[key]    
