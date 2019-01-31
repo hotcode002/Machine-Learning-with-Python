942f3309c44784e81ae2dea8a9ffdf342f983804
Ajay Tech <siva@ajaytech.co>
2019-01-31 17:05:44 +0530

    statistics notebook

--- week_1_basics/day_1_get_your_feet_wet/test.py
+++ week_1_basics/day_1_get_your_feet_wet/test.py
@@ -1,29 +1,8 @@
-import numpy as np
+import random
+import math
 
-a = np.arange(20).reshape(5,4)
-print ( a)
+random_numbers = []
+for i in range(100) :
+    random_numbers.append(math.trunc( random.random() * 100 ))
 
-b = np.arange(20,40).reshape(5,4)
-
-print (b)
-c = a>5
-d = a<15
-
-e = c & d
-print ( e)
-print (a[e])
-
-# c = a[a>5 & a<15]
-# print ( c )
-
-# d = a * b
-
-# print ( c)
-
-# print ( d)
-
-# a_10 = np.full ( (3,3),True)
-# a_5  = np.full ( (3,3), False )
-
-# a_5_v = np.array([5,10,15])
-# print ( a_10 & a_5 )
+print ( random_numbers)
