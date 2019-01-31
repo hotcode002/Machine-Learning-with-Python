5f31eb1f123cdf9130f499bd213e07a2a66bb908
Ajay Tech <siva@ajaytech.co>
2019-01-31 19:53:01 +0530

    statistics part 1

--- week_1_basics/day_1_get_your_feet_wet/test.py
+++ week_1_basics/day_1_get_your_feet_wet/test.py
@@ -2,7 +2,7 @@ import random
 import math
 
 random_numbers = []
-for i in range(100) :
-    random_numbers.append(math.trunc( random.random() * 100 ))
+for i in range(1000) :
+    random_numbers.append(math.trunc( random.uniform(50,98) ))
 
 print ( random_numbers)
