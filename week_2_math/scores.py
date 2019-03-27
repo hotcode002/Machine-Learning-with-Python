78d1a6e8601f7c87fbddacef0652d309bb99b2ce
Ajay Tech <siva@ajaytech.co>
2019-03-27 19:13:31 +0530

    statistics

--- week_2_math/scores.py
+++ week_2_math/scores.py
@@ -1,20 +1,14 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
-# Read the scores file
-scores = np.genfromtxt("./data/gre_scores_3_sets.csv",delimiter=",")
+i = np.random.randn(1000) * 10
 
-f, (ax1, ax2,ax3) = plt.subplots(1, 3, sharey=True)
-ax1.set_xlim(200,400)
-ax1.axvline(x=305, ymin=0, ymax = 800, linewidth=2, color='r')
-ax1.axvline(x=300, ymin=0, ymax = 800, linewidth=2, color='g')
-ax1.hist(scores[:,0],bins=300)
-ax2.set_xlim(200,400)
-ax2.axvline(x=315, ymin=0, ymax = 800, linewidth=2, color='r')
-ax2.axvline(x=300, ymin=0, ymax = 800, linewidth=2, color='g')
-ax2.hist(scores[:,1],bins=300)
-ax3.set_xlim(200,400)
-ax3.axvline(x=330, ymin=0, ymax = 800, linewidth=2, color='r')
-ax3.axvline(x=300, ymin=0, ymax = 800, linewidth=2, color='g')
-ax3.hist(scores[:,2],bins=300)
+i = np.sort(i)
+
+print(i)
+
+plt.boxplot(i)
+plt.show()
+
+plt.hist(i, bins=50)
 plt.show()
