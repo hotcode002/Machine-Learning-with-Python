9756c16132a2264d6bd2577aa4d378412f05d750
Ajay Tech <siva@ajaytech.co>
2019-03-29 15:13:59 +0530

    file structure change

---
+++ week_1_python/day_1_get_your_feet_wet/.ipynb_checkpoints/3_average_grade_v1-checkpoint.py
@@ -0,0 +1,24 @@
+##########################################################################
+# Program is copyright of Ajay Tech @ https://ajaytech.co
+##########################################################################
+# Calculate mean without using list data structure
+# In this program, we will learn about 
+# 1. int vs float
+
+print ( "Enter grades to calculate mean/average grade. type e to exit")
+sum     = 0
+mean    = 0
+count   = 0
+while True :
+    grade = input ( " - ")
+    if grade == "e" :
+        break
+    else :
+        grade   = float(grade)
+        sum     = sum + grade
+        count   = count + 1
+
+mean = sum/count
+
+print ( "Average class grade is ",mean)
+
