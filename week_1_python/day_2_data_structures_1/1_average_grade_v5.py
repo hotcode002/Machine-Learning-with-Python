3b2f6b04cf7e91f17c1f4551ea074b9d85477766
Ajay Tech <siva@ajaytech.co>
2019-04-04 20:21:49 +0530

    lists

--- week_1_python/day_2_data_structures_1/1_average_grade_v5.py
+++ week_1_python/day_2_data_structures_1/1_average_grade_v5.py
@@ -1,16 +1,10 @@
 ##########################################################################
 # Program is copyright of Ajay Tech @ https://ajaytech.co
 ##########################################################################
-# This is an extension of the previous program - except
-# we will rank each school by their average grade and 
-# list it in that order, along with the respective grades
-# for example, 
-# "Sherwood Elementery school", 3.5, [3.5, 4.0, 2.9 ...]
-# "Redmond Elementery school" , 3.4, [3.2, 4.1 ,3.5 ...]
-# .. so on
-
-# We could do this using 2 lists more easily, but let's work everything
-# into a single list and see if we do this.
+# Say you are calculating the average grade across the entire district
+# Each school has a separate list of grades, we just have to calculate 
+# the average across all the lists( school ). Just for simplicity, let's 
+# assume we are calculating grades for just Class 1 across all the schools
 
 # In this program, we will learn about 
 # 1. merge lists using the + operator
@@ -19,14 +13,9 @@ import math
 
 grades_each_schools = []
 
-# say there are 5 schools and we are calculating
-# the average grade for just one class
-
-for i in range(1,6) :
-    school_name = input (" Enter the school name - ") 
-    school_grades = []
-    school_grades.append(school_name)
-    print ( "Enter the grades for this school (e to exit ) -")
+# say there are 5 schools
+for i in range(1,6) : 
+    print ( "Enter grades of each student for school ",i," - 'e' to exit")
     grades = []
     while True :
         grade = input ( " - ")
@@ -34,25 +23,14 @@ for i in range(1,6) :
             break
         else :
             grades.append(float(grade))
-    school_grades.append(grades)
-    grades_each_schools.append(school_grades)
+    
+    grades_each_schools.append(grades)
 
-print ( grades_each_schools)
+grades_all_schools = []
+for grades in grades_each_schools :
+    grades_all_schools += grades
 
-for school in grades_each_schools :
-    average = math.fsum(school[1]) / len(school[1])
-    school.insert(0,average)
+average = math.fsum(grades_all_schools) / len(grades_all_schools)
+print ( "Average grade of all 5 schools is - ", average)
 
-print ( grades_each_schools)
 
-# sort the school by average grade in reverse order
-grades_each_schools.sort( reverse = True)
-
-# pretty up the printing
-# for school in grades_each_schools :
-#     print (school[0],"\t\t",school[1],"\t\t\t\t",school[2])
-
-# or better
-print ('{:<10} {:<50} {:<30} '.format("Avg. Grade","School Name","Grades") )
-for school in grades_each_schools :
-    print ('{:<10} {:<50} {:<30} '.format(school[0],school[1],str(school[2])) )
